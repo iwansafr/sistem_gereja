e4df5d6e48965fbb13646f5924591a543e0cef6c
iwansafr <iwansafr@gmail.com>
2021-02-05 23:33:29 +0700

    download akta

---
+++ application/modules/home/controllers/Akta.php
@@ -0,0 +1,19 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Akta extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('home_model');
+		$this->load->helper('content');
+		$this->load->library('ZEA/Zea');
+		$this->load->library('esg');
+	}
+	public function index()
+	{
+		$user = $this->session->userdata(base_url('_logged_in'));
+		$data = $this->db->get_where('akta',['user_id'=>$user['id']])->result_array();
+		$this->load->view('index',['data'=>$data,'user_id'=>$user['id']]);
+	}
+}
